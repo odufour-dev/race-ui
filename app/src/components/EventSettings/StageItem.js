8325e90dcbf3ad405a6d6c8119cf0be24d7a50ad
Olivier DUFOUR <odufour.dev@gmail.com>
2025-11-03 23:15:05 +0100

    feat: Improve rendering for Event Settings, especially for stages

--- app/src/components/EventSettings/StageItem.js
+++ app/src/components/EventSettings/StageItem.js
@@ -9,6 +9,7 @@ export default function StageItem({ translator, stage, onRemove }) {
     <div className="stage-item">
       <div className="stage-header">
         <div className="stage-h-left">
+            <span>{translator("event.settings.stage.stage")} #{ item.id }</span>
           <input
             className="stage-name"
             placeholder={translator('event.settings.stage.name')}
@@ -29,11 +30,11 @@ export default function StageItem({ translator, stage, onRemove }) {
             value={item.distance ?? 0}
             onChange={e => setItem( item.update({distance: e.target.value }))}
             title={translator('event.settings.stage.distance')}
-          />
+          /><span>km</span>
         </div>
 
         <div className="stage-h-right">
-          <span>{translator("event.settings.stage.stage")} #{ item.id }</span>
+          
           <button type="button" className="btn small danger" onClick={() => onRemove && onRemove(item.id)}>{translator('event.settings.stage.remove')}</button>
         </div>
       </div>
