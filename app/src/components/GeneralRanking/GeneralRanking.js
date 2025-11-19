046eed1d39a90724fcab1210bac27aefc76cabf1
Olivier DUFOUR <odufour.dev@gmail.com>
2025-11-19 23:15:33 +0100

    feat(WIP): Create UI component for General ranking

---
+++ app/src/components/GeneralRanking/GeneralRanking.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+import './GeneralRanking.css';
+
+export default function GeneralRanking({ /*data = [],*/ time, translator }) {
+
+    const data = [
+        {bib: 2, position: 4, time: 2345, status: "done", stage: 2, firstname: "Paul", lastname: "POULE", club: "GMC 38", category: "elite", age: "u23"},
+    ];
+
+    const [ columns, setColumns ]   = useState( ["bib", "time", "firstname", "lastname", "club", "category", "age", "ffcid", "uciid" ] );
+    const [ ranking, setRanking ]   = useState( data );
+
+    useEffect(() => {
+        setRanking(data.map((d) => columns.map((c) => c in d ? d[c] : "")));
+    }, [ columns, data ]);
+
+    return (
+        <div className="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>{translator("column.position")}</th>
+                        {columns.map(col => (<th key={col}>{translator("column." + col)}</th>))}
+                    </tr>
+                </thead>
+                <tbody>
+                {ranking.map(row => (
+                    <tr key={"position-" + row.position}>
+                        <td>{row.position}</td>
+                        {Object.keys(row).map((d) => (<td>{row[d]}</td>))}
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
