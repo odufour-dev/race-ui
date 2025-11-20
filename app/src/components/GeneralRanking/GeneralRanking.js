a6ff93615707b29ffabad407f7df9469aca20bc8
Olivier DUFOUR <odufour.dev@gmail.com>
2025-11-20 22:43:08 +0100

    feat(WIP): Display General Ranking

--- app/src/components/GeneralRanking/GeneralRanking.js
+++ app/src/components/GeneralRanking/GeneralRanking.js
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from 'react';
 
 import './GeneralRanking.css';
 
-export default function GeneralRanking({ /*data = [],*/ time, translator }) {
-
+export default function GeneralRanking({ data = [], time, translator }) {
+/*
     const data = [
         {bib: 2, position: 4, time: 2345, status: "done", stage: 2, firstname: "Paul", lastname: "POULE", club: "GMC 38", category: "elite", age: "u23"},
     ];
-
+*/
     const [ columns, setColumns ]   = useState( ["bib", "time", "firstname", "lastname", "club", "category", "age", "ffcid", "uciid" ] );
     const [ ranking, setRanking ]   = useState( data );
 
     useEffect(() => {
-        setRanking(data.map((d) => columns.map((c) => c in d ? d[c] : "")));
+        let ranking = data.map((d) => columns.map((c) => c in d ? d[c] : ""));
+        ranking = ranking.map((r) => r.time != "" ? time.formatHMS(r.time) : "");
+        setRanking(ranking);
     }, [ columns, data ]);
 
     return (
