3ef3f483bfd0411447cc30999183f1ca0d3f3792
Olivier DUFOUR <odufour.dev@gmail.com>
2026-02-04 23:16:45 +0100

    feat(WIP): Create a Components class to isolate from Navigation menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,36 @@
 import './App.css';
 
-import React, { useMemo, useEffect, useState, useCallback, startTransition } from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useMemo, useEffect, useState, useCallback }  from 'react';
+import { useTranslation }                       from 'react-i18next';
 
-import APIConnection      from './components/APIConnection/APIConnection';
-import { NavigationMenu } from './components/NavigationMenu';
+import APIConnection                            from './components/APIConnection/APIConnection';
+import { createComponents }                     from './components/Components';
+import { NavigationMenu }                       from './components/NavigationMenu';
 
-import { RaceConnector }  from './models/RaceConnector';
-import { Metadata }       from './models/Metadata/Metadata';
+import { RaceConnector }                        from './models/RaceConnector';
+import { Metadata }                             from './models/Metadata/Metadata';
 
-import { Helper }         from './tools/Helper';
+import { Helper }                               from './tools/Helper';
 
 function App() {
 
   const { t: translator } = useTranslation('translation');
   const helper = new Helper(translator);
 
-  const [appName, setAppName] = useState("");
+  const [appName, setAppName]           = useState("");
   const [selectedItem, setSelectedItem] = useState(null);
+  const [components, setComponents]     = useState(null);
 
   const [competitionid, setCompetitionId] = useState("");
   const connector = useMemo(() => new RaceConnector("/api/v1", console), []);
+
+  const handleInit = (competitionid) => {
+      createComponents({ helper, connector, competitionid })
+          .then(setComponents); // Met à jour l'état quand c'est prêt
+  }
+  useEffect(() => {
+        handleInit(competitionid);
+    }, [competitionid]);
 
   useEffect(() => {
     const metadata = new Metadata();
@@ -29,7 +39,7 @@ function App() {
       setAppName(name);
     });
   }, []);
-
+  
   return (
     <div className="App" style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
       <APIConnection helper={helper} connector={connector} value={competitionid} onChange={setCompetitionId} />
@@ -37,9 +47,10 @@ function App() {
         <NavigationMenu helper={helper} connector={connector} competitionid={competitionid} onSelect={setSelectedItem}/>
       </div>
       <main style={{ flex: 1, overflow: 'auto', padding: '1rem' }}>
-        {selectedItem && selectedItem.component ? (selectedItem.component()) : (
-          <div style={{ padding: '1rem', color: '#334155' }}>Select a view from the left navigation.</div>
-        )}
+        { components ? 
+            components.renderById("event-settings",competitionid) : 
+            (<div style={{ padding: '1rem', color: '#334155' }}>Select a view from the left navigation.</div>)
+        }
       </main>
       <footer>{appName}</footer>
     </div>
@@ -47,3 +58,11 @@ function App() {
 }
 
 export default App;
+
+/*
+<main style={{ flex: 1, overflow: 'auto', padding: '1rem' }}>
+  {selectedItem && selectedItem.component ? (selectedItem.component()) : (
+    <div style={{ padding: '1rem', color: '#334155' }}>Select a view from the left navigation.</div>
+  )}
+</main>
+*/
---
+++ frontend/src/components/Components.js
@@ -0,0 +1,113 @@
+
+import EventSettings      from './EventSettings/EventSettings';
+import ExcelReader        from './ExcelReader/ExcelReader';
+import GeneralRanking     from './GeneralRanking/GeneralRanking';
+import SynchronizationBar from './Synchronization/SynchronizationBar';
+import RegistrationTable  from './RegistrationTable/RegistrationTable';
+import StageRanking       from './StageRanking/StageRanking';
+
+class Component {
+
+    #id
+    #component
+    #connector
+    #helper
+
+    constructor(helper,connector,id,component){
+        this.#id        = id;
+        this.#component = component;
+        this.#connector = connector;
+        this.#helper    = helper;
+    }
+
+    get id(){return this.#id;}
+
+    render(competitionid){
+        return this.#component(this.#helper,this.#connector,competitionid);
+    }
+    
+}
+
+class Components {
+
+    #components
+    #connector
+    #helper
+
+    constructor(helper,connector){
+        this.#helper        = helper;
+        this.#connector     = connector;
+        this.#components    = [];
+    }
+
+    add(id,component){
+        this.#components.push(new Component(this.#helper,this.#connector,id,component));
+    }
+
+    renderById(id,competitionid){
+        const comp = this.#components.find((c) => c.id === id);console.log(this.#components,comp);
+        if (comp){
+            return comp.render(competitionid);
+        } else {
+            return (<div style={{ padding: '1rem', color: '#334155' }}>Cannot find view "{id}" for competition "{competitionid}"</div>);
+        }
+    }
+
+}
+
+export async function createComponents({helper, connector, competitionid }){
+
+    const createStageRanking = (stage) => {
+        return (helper,connector,competitionid) => (
+            <StageRanking
+            competitionid={competitionid}
+            stage={stage}
+            connector={connector}
+            helper={helper}
+            />
+        );
+    }
+
+    const createGeneralRanking = (stage) => {
+        return (helper,connector,competitionid) => (
+            <GeneralRanking
+            competitionid={competitionid}
+            stage={stage}
+            connector={connector}
+            helper={helper}
+            />
+        );
+    }
+
+    const components = new Components(helper,connector);
+
+    if (competitionid){
+
+        const configuration = await connector.fetchConfiguration(competitionid);
+
+        components.add("event-settings", (helper,connector,competitionid) => (
+            <EventSettings  
+                helper={helper} 
+                connector={connector} 
+                competitionid={competitionid} 
+                savebar={(hasUnsavedChanges,onSave) => (<SynchronizationBar hasUnsavedChanges={hasUnsavedChanges} onSave={onSave} />)} />
+        ));
+
+        components.add("registration-table", (helper,connector,competitionid) => (
+            <RegistrationTable 
+                helper={helper} 
+                connector={connector} 
+                competitionid={competitionid} 
+                savebar={(hasUnsavedChanges,onSave) => (<SynchronizationBar hasUnsavedChanges={hasUnsavedChanges} onSave={onSave} />)} />
+        ));
+
+        for (let stage = 1; stage <= configuration.nStages; stage++) {
+            components.add("stage-"     + stage,  createStageRanking(stage));
+            components.add("general-"   + stage,  createGeneralRanking(stage));
+        }
+
+    }
+
+    return components;
+
+}
