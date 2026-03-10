c908a6f679eff6a9893caca1beeace7dad9759f6
Olivier DUFOUR <odufour.dev@gmail.com>
2026-03-10 10:13:16 +0100

    feat: Add 'order' option for general route with 'bib' option

--- backend/src/routes/generalranking.js
+++ backend/src/routes/generalranking.js
@@ -11,6 +11,7 @@ export default class GeneralRanking extends Route {
 
         const compid  = req.params.id;
         const stageNumber = parseInt(req.params.stageId, 10);
+        const order = req.query.order;
 
         try {
 
@@ -18,44 +19,61 @@ export default class GeneralRanking extends Route {
             if (!competition){return res.status(404).json({status: "notfound", message: "Database not found for " + compid});}
             
             const groupresults = {};
-            for (let istage = 1; istage <= stageNumber; istage++){
-                const stageresults = await this._processor.extractRankingForStage(competition.database,competition.raceId,istage);
-                stageresults.forEach(result => {
 
-                    if (groupresults[result.bib] && result.status !== "unknown" && result.status !== "abs"){
-                        groupresults[result.bib].time       += result.time - groupresults[result.bib].bonification;
-                        groupresults[result.bib].cumulated  += result.rank;
+            for (let istage = 1; istage <= stageNumber; istage++) {
+                const stageresults = await this._processor.extractRankingForStage(competition.database, competition.raceId, istage);
+                
+                stageresults.forEach(result => {
+                    // Calcul du temps net pour CETTE étape
+                    const stageTimeNet = (result.time || 0) - (result.bonification || 0);
+
+                    if (!groupresults[result.bib]) {
+                        // Initialisation propre au premier passage du coureur
+                        groupresults[result.bib] = {
+                            ...result,
+                            time: stageTimeNet, // On commence avec le temps net de la 1ère étape
+                            cumulated: result.rank,
+                            stage: istage
+                        };
+                    } else if (result.status !== "unknown" && result.status !== "abs") {
+                        // Mise à jour pour les étapes suivantes
+                        groupresults[result.bib].time      += stageTimeNet;
+                        groupresults[result.bib].cumulated += result.rank;
                         groupresults[result.bib].rank       = result.rank;
                         groupresults[result.bib].status     = result.status;
                         groupresults[result.bib].stage      = istage;
-                    } else if (istage == 1 && !groupresults[result.bib]) {
-                        groupresults[result.bib] = {...result, stage: istage, cumulated: result.rank};
-                        groupresults[result.bib].time -= groupresults[result.bib].bonification;
                     }
-
+                    //if (result.bib == 161){console.log(groupresults[result.bib])}
                 });
             }
 
-            const allresults = Object.values(groupresults).sort((a, b) => {
-                // If final status is not done, consider that racer does not finish => go to the end
-                // Sort non-finisher based on the last recorded stage (higher first)
-                if (a.status == "done" && b.status != "done"){return -1;}
-                else if (a.status != "done" && b.status == "done"){return 1;}
-                else if (a.stage != b.stage){return b.stage - a.stage;}
-                // Finishers (status == done)
-                //  1. Compare the time (if different)
-                //  2. Compare the milliseconds (if different)
-                //  3. Compare the cumulated positions
-                //  4. Compare the last position
-                else if (a.time != b.time){return a.time - b.time;}
-                else if (a.millis != b.millis){return a.millis - b.millis;}
-                else if (a.cumulated != b.cumulated){return a.cumulated - b.cumulated;}
-                else {return a.last - b.last;}
-            });
+            let allresults = [];
+            if (order && order == "bib"){
+                allresults = Object.values(groupresults).sort((a, b) => a.bib - b.bib);
+            } else {
+                allresults = Object.values(groupresults).sort((a, b) => {
+                    // If final status is not done, consider that racer does not finish => go to the end
+                    // Sort non-finisher based on the last recorded stage (higher first)
+                    if (a.status == "done" && b.status != "done"){return -1;}
+                    else if (a.status != "done" && b.status == "done"){return 1;}
+                    else if (a.stage != b.stage){return b.stage - a.stage;}
+                    // Finishers (status == done)
+                    //  1. Compare the time (if different)
+                    //  2. Compare the milliseconds (if different)
+                    //  3. Compare the cumulated positions
+                    //  4. Compare the last position
+                    else if (a.time != b.time){return a.time - b.time;}
+                    else if (a.millis != b.millis){return a.millis - b.millis;}
+                    else if (a.cumulated != b.cumulated){return a.cumulated - b.cumulated;}
+                    else {return a.last - b.last;}
+                });
+                
+            }
 
+            const results = allresults.map(r => ({...r, time: this._time.formatHMS(r.time), delay: this._time.formatHMS(r.time - allresults[0].time)}));
             res.status(200).json({
                 stage: stageNumber,
-                results: allresults.map(r => ({...r, time: this._time.formatHMS(r.time), delay: this._time.formatHMS(r.time - allresults[0].time)}))
+                results: results
             });
 
         } catch (error) {
